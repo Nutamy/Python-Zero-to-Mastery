78df026e19bcd5f8d0fdf9a58bdff12c7ab8234c
Nataliya Kozulina <natasha.pechenka@gmail.com>
2021-11-30 21:58:01 +0600

    Create dictionary_methods_2.py

---
+++ dictionary_methods_2.py
@@ -0,0 +1,36 @@
+dic = {
+  'a': [1,2,3],
+  'b': 'hello',
+  'c': 12
+}
+
+for i in dic:
+  print(i)
+
+print(dic.get('a'))
+print(dic.get('z'))
+
+return_get_dic_a = dic.get('a', 'True')
+return_get_dic_z= dic.get('z', 'True')
+keys = dic.keys()
+print(keys)
+
+for i in keys:
+  print(i)
+
+values = dic.values()
+
+print(values)
+
+for i in values:
+  print(i)
+
+items = dic.items()
+
+print(items)
+
+for i in items:
+  print(i)
+
+#dic.clear()
+print(dic.popitem())
